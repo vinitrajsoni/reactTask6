b829b98ff980f02a551c88b2e24eab6ff4a024d4
vinitrajsoni <131142169+vinitrajsoni@users.noreply.github.com>
2023-10-17 13:13:36 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Harsh_Mendhe_ Resume.pdf";
+import pdf from "../../Assets/VINIT RAJ SONI.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
